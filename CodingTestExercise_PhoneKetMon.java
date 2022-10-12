d21dd65e9be7e93e8aea8b2d877295e8fb73776f
yoonjin99 <60498414+yoonjin99@users.noreply.github.com>
2022-10-12 16:18:26 +0900

    Update CodingTestExercise_PhoneKetMon.java

--- CodingTestExercise_PhoneKetMon.java
+++ CodingTestExercise_PhoneKetMon.java
@@ -2,19 +2,14 @@
 import java.util.*;
 class Solution {
     public int solution(int[] nums) {
-        int pickPocketMonNumber = nums.length / 2;
-
         HashSet<Integer> distinctNums = new HashSet<>();
         for(int num : nums){
             distinctNums.add(num);
         }
-
+        
+        int pickPocketMonNumber = nums.length / 2;
         int typeNumber = distinctNums.size();
 
-        if(pickPocketMonNumber > typeNumber){
-            return typeNumber;
-        }else{
-            return pickPocketMonNumber;
-        }
+        return Math.min(pickPocketMonNumber, typeNumber);
     }
 }
